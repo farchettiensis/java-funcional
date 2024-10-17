cc4515656615ad4b5602710de4b43ecc8e8b5a25
farchettiensis <farchettiensis@users.noreply.github.com>
2024-10-16 23:48:10 -0300

    Ordenador de lista

--- src/lista2/OrdenadorLista.java
+++ src/lista2/OrdenadorLista.java
@@ -1,4 +1,18 @@
 package lista2;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OrdenadorLista {
+    public static void main(String[] args) {
+        int[] array = {5, 3, 8, 1, 2};
+        int[] arrayOrdenado = ordenarListaComLambda(array);
+        System.out.println(Arrays.toString(arrayOrdenado));
+    }
+
+    public static int[] ordenarListaComLambda(int[] array) {
+        List<Integer> lista = Arrays.stream(array).boxed().sorted((a, b) -> a - b).collect(Collectors.toList());
+        return lista.stream().mapToInt(i -> i).toArray();
+    }
 }
